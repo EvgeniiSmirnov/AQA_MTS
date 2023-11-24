ef70a53c900efd269f168ddaee3679e9c1f5af82
HappyLemon <sm.itsmyworld@gmail.com>
2023-11-24 19:55:14 +0500

    + Задание 1

---
+++ Homework2Exc1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задание 1.
+Создайте массив целых чисел.
+Напишете программу, которая выводит сообщение о том, входит ли заданное число в массив или нет.
+Пусть число для поиска задается с консоли.
+*/
+
+namespace Homework2Exc1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Создаём массив
+            int[] array = new int[10];
+            Random rnd = new();
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = rnd.Next(51); //помещаем в массив случайное число от 0 до 50
+            }
+
+            // Расскомментировать, чтобы посмотреть созданный массив
+            //foreach (int i in array)
+            //{
+            //    Console.Write(i + "\t");
+            //}
+
+            Console.WriteLine("Введите число:");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Проверяем элементы массива и выводим сообщение
+            Console.WriteLine($"Число '{userNumber}' " + (array.Contains(userNumber) ? "содержится" : "не содержится") + " в массиве");
+
+        }
+    }
+}
