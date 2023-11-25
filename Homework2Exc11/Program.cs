6e3816cea7fe1a633458444a3c073cd4d696f55a
HappyLemon <sm.itsmyworld@gmail.com>
2023-11-25 19:02:22 +0500

    сократил 11 задание

--- Homework2Exc11/Program.cs
+++ Homework2Exc11/Program.cs
@@ -15,36 +15,22 @@ namespace Homework2Exc11
             int arraySize = rnd.Next(10, 20);
             int[,] array = new int[arraySize, arraySize];
 
-            // инициализируем случайными числами
+            // массируем ヽ(°° )ノ массив 
+            string firstDiagonal = "", secondDiagonal = "";
             for (int i = 0; i < arraySize; i++)
             {
                 for (int j = 0; j < arraySize; j++)
                 {
-                    array[i, j] = rnd.Next(10);
-                }
-            }
-
-            // выводим массив в консоль
-            for (int i = 0; i < array.GetLength(0); i++) // GetLength(0) - количество строк
-            {
-                for (int j = 0; j < array.GetLength(1); j++) // GetLength(1) - количество столбцов
-                {
-                    Console.Write(array[i, j] + "  ");
-                }
-                Console.WriteLine();
-            }
-
-            // ищем диагонали
-            string firstDiagonal = "", secondDiagonal = "";
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
+                    array[i, j] = rnd.Next(10);         // инициализируем случайными числами
+                    Console.Write(array[i, j] + "  ");  // выводим массив в консоль
+                    // первая диагональ
                     if (i == j)
                         firstDiagonal += array[i, j] + " ";
+                    // вторая диагональ
                     if (i + j == arraySize - 1)
                         secondDiagonal += array[i, j] + " ";
                 }
+                Console.WriteLine();
             }
 
             Console.WriteLine($"Первая (начало в 00) диагональ: {firstDiagonal}\nВторая диагональ: {secondDiagonal}");
