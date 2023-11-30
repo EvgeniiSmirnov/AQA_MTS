7f869e90bc72e5e1e1605f188232ec27cbcd6299
HappyLemon <sm.itsmyworld@gmail.com>
2023-11-30 18:34:50 +0500

    + задание 6

---
+++ Homework3Task6/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задание 6
+Дана строка (максимально 100 символов), содержащая слова, разделенные одним или несколькими пробелами, или знаками табуляции.
+Заменить все знаки табуляции знаком пробела, удалить двойные пробелы из строки.
+При реализации программы функции из библиотеки System.String не использовать.
+*/
+
+using System.Text.RegularExpressions;
+
+namespace Homework3Task6
+{
+    public class Program
+    {
+        static void Main()
+        {
+            {
+                string text = "1. Some text with  some test    words. 2. There first test       word or what? 999.      Something   here too.";
+                Console.WriteLine("Текст до изменений:\n" + text);
+
+                string newText = Regex.Replace(text, @"\s+", " ");
+                Console.WriteLine("Текст после изменений:\n" + newText);
+            }
+        }
+    }
+}
