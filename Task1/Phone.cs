10efba8134e84f26b0094040a08d5dabd72c1643
HappyLemon <sm.itsmyworld@gmail.com>
2023-12-02 23:26:03 +0500

    + Task 1

---
+++ Task1/Phone.cs
@@ -0,0 +1,39 @@
+﻿namespace Task1;
+public class Phone
+{
+    public string number;
+    public string model;
+    public double weight;
+
+    public Phone()
+    {
+        number = "79001122333";
+        model = "testModel";
+        weight = 0;
+    }
+
+    public Phone(string number, string model)
+    {
+        this.number = number;
+        this.model = model;
+        weight = 0;
+    }
+
+    public Phone(string number, string model, double weight) : this(number, model)
+    {
+        this.weight = weight;
+    }
+
+    // метод выводит на консоль сообщение “Звонит {name}”
+    public void receiveCall(string callerName) => Console.Write($"Звонит {callerName}");
+
+    // метод возвращает номер телефона
+    public string getNumber() => number;
+
+    // метод выводит на консоль номера телефоном которым будет отправлено сообщение
+    public void sendMessage(params string[] numbers)
+    {
+        foreach (string number in numbers)
+            Console.WriteLine($"Сообщение будет отправлено на номер {number}");
+    }
+}
