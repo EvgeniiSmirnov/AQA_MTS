4de198d81ed9dd515abe7e46d81a9b63be5c2ae9
HappyLemon <sm.itsmyworld@gmail.com>
2023-12-24 15:01:32 +0500

    + task 1

---
+++ Homework7Task1/LoginPassword.cs
@@ -0,0 +1,73 @@
+﻿using Homework7Task1.Exceptions;
+using System.Text.RegularExpressions;
+
+namespace Homework7Task1;
+
+internal class LoginPassword
+{
+    private static string Login { get; set; }
+
+    private static string Password { get; set; }
+
+    private static string ConfirmPassword { get; set; }
+
+    public static bool CheckLoginPassData(string login, string password, string confirmPassword)
+    {
+        Login = login;
+        Password = password;
+        ConfirmPassword = confirmPassword;
+
+        try
+        {
+            // ветка проверки логина
+            // длина login должна быть меньше 20 символов и не должен содержать пробелы
+            if (string.IsNullOrEmpty(Login))
+            {
+                throw new WrongLoginException("Ошибка! login не задан");
+            }
+            else if (Login.Contains(' '))
+            {
+                throw new WrongLoginException("Ошибка! login содержит пробелы");
+            }
+            else if (Login.Length >= 20)
+            {
+                throw new WrongLoginException("Ошибка! Длина login должна быть меньше 20 символов");
+            }
+
+            // если с логином всё хорошо пападаем в ветку проверки пароля
+            // длина password должна быть меньше 20 символов, не должен содержать пробелом и должен содержать хотя бы одну цифру
+            else
+            {
+                Regex regex = new(@"^\d+$");
+                if (string.IsNullOrEmpty(Password))
+                {
+                    throw new WrongPasswordException("Ошибка! password не задан");
+                }
+                else if (Password.Contains(' '))
+                {
+                    throw new WrongPasswordException("Ошибка! password содержит пробелы");
+                }
+                else if (Password.Length >= 20)
+                {
+                    throw new WrongPasswordException("Ошибка! Длина password должна быть меньше 20 символов");
+                }
+                else if (!regex.IsMatch(Password))
+                {
+                    throw new WrongPasswordException("Ошибка! password должен содержать хотя бы одну цифру");
+                }
+                else if (!Password.Equals(ConfirmPassword))
+                {
+                    throw new WrongPasswordException("Ошибка! password не совпадает с confirmPassword");
+                }
+
+                return true;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+}
--- Homework7Task1/Program.cs
+++ Homework7Task1/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 1:
+Создать класс, в котором будет статический метод. Этот метод принимает на вход три параметра:
+login, password, confirmPassword.
+
+Все поля имеют тип данных String.
+- Длина login должна быть меньше 20 символов и не должен содержать пробелы.
+- Если login не соответствует этим требованиям, необходимо выбросить WrongLoginException.
+- Длина password должна быть меньше 20 символов, не должен содержать пробелом и должен содержать хотя бы одну цифру.
+- Также password и confirmPassword должны быть равны.
+- Если password не соответствует этим требованиям, необходимо выбросить WrongPasswordException.
+
+WrongPasswordException и WrongLoginException - пользовательские классы исключения с двумя конструкторами – один по умолчанию,
+второй принимает сообщение исключения и передает его в конструктор класса Exception.
+Метод возвращает true, если все значения верны или false в другом случае.
+*/
+
+using Homework7Task1;
+
+public class Program
+{
+    public static void Main()
+    {
+        // для проверки создадим массив с различными входными данными
+        bool[] test =
+        {
+            // негативные проверки логина
+            LoginPassword.CheckLoginPassData("", "123", "123"),
+            LoginPassword.CheckLoginPassData("alex ", "123", "123"),
+            LoginPassword.CheckLoginPassData("alexalexalexalexalexalexalex", "123", "123"),
+
+            // негативные проверки пароля
+            LoginPassword.CheckLoginPassData("alex", "", "123"),
+            LoginPassword.CheckLoginPassData("alex", "123 ", "123"),
+            LoginPassword.CheckLoginPassData("alex", "alex", "123"),
+            LoginPassword.CheckLoginPassData("alex", "124", "123"),
+
+            // позитивная проверка
+            LoginPassword.CheckLoginPassData("alex", "123", "123")
+        };
+
+        for (int i = 0; i < test.Length; i++)
+        {
+            Console.Write($"test {i}: ");
+            if (!test[i])
+            {
+                Console.WriteLine("Данные не приняты");
+            }
+            else
+            {
+                Console.WriteLine("Данные приняты");
+            };
+        }
+    }
+}
