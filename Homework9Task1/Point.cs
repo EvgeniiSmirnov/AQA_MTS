2b2e969e7a238f5509513e3d4090bef298940e5d
HappyLemon <sm.itsmyworld@gmail.com>
2024-01-14 14:02:57 +0500

    + task 1

---
+++ Homework9Task1/Point.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework9Task1;
+
+internal class Point<T>
+{
+    private T _x;
+    private T _y;
+    public T X { get => _x; set => _x = value; }
+    public T Y { get => _y; set => _y = value; }
+
+    public Point(T x, T y)
+    {
+        _x = x;
+        _y = y;
+    }
+
+    public void Print() => Console.WriteLine($"({_x}, {_y})");
+}
--- Homework9Task1/Program.cs
+++ Homework9Task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 1:
+Реализовать класс Point, который определяет точку на координатной плоскости. В классе реализовать:
+- внутренние поля x, y;
+- конструктор с 2 параметрами;
+- свойства доступа к внутренним полям класса;
+- метод, выводящий значения внутренних полей класса.
+*/
+
+using Homework9Task1;
+class Program
+{
+    public static void Main()
+    {
+        // создадим разнотипные объекты
+        Point<int> pointInt = new(10, 20);
+        Point<decimal> pointDecimal = new(30m, 40m);
+        Point<string> pointString = new("50", "60");
+        
+        // проверка метода
+        pointInt.Print();
+        pointDecimal.Print();
+
+        // проверка свойств
+        Console.WriteLine($"({pointString.X}, {pointString.Y})");
+        Console.WriteLine($"({pointInt.X=15}, {pointInt.Y=30})");
+    }
+}
