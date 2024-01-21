d488fd45fbfc7bb7cd8dc3f4c21eeff935ba5e0b
HappyLemon <sm.itsmyworld@gmail.com>
2024-01-21 13:58:23 +0500

    + task 1

--- Homework10Task1/Program.cs
+++ Homework10Task1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 1:
+Для приложения объявить тип делегата, который ссылается на метод.
+Требования к сигнатуре метода следующие:
+- метод не имеет входных параметров;
+- метод возвращает int значение.
+Реализовать вызов метода с помощью делегата. Метод возвращает случайное значение от 0 до 10.
+*/
+
+class Program
+{
+    // объявляем делегат
+    private delegate int Number();
+
+    static void Main()
+    {
+        // инициализируем делегат
+        Number number = GetRandomNumber;
+
+        // реализуем метод с помощью делегата
+        Console.WriteLine(number());
+    }
+
+    /// <summary>
+    /// Метод возвращает случайное значение от 0 до 10
+    /// </summary>
+    /// <returns></returns>
+    private static int GetRandomNumber()
+    {
+        return new Random().Next(10);
+    }
+}
