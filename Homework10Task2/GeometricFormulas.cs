358725a99c5d382cfa2deab4b3e7f3f5fdc6c305
HappyLemon <sm.itsmyworld@gmail.com>
2024-01-21 14:53:22 +0500

    + task 2

---
+++ Homework10Task2/GeometricFormulas.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework10Task2;
+
+internal class GeometricFormulas
+{
+    const double PI = Math.PI;
+
+    /// <summary>
+    /// Метод для рассчёта длины окружности
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+    public double GetCircleLength(double radius) => 2 * PI * radius;
+
+    /// <summary>
+    /// Метод для рассчёта площади круга
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+    public double GetCircleSquare(double radius) => PI * Math.Pow(radius, 2);
+
+    /// <summary>
+    /// Метод для рассчёта объёма шара
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+    public double GetSphereVolume(double radius) => PI * Math.Pow(radius, 3) * 4 / 3;
+}
--- Homework10Task2/Program.cs
+++ Homework10Task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 2:
+Для приложения объявить тип делегата, который ссылается на метод. Требования к сигнатуре метода следующие:
+- метод получает входным параметром переменную типа double;
+- метод возвращает значение типа double, которое есть результатом вычисления в соответствии с условием задачи.
+Реализовать вызов трех методов с помощью делегата, которые получают радиус R в качестве входного параметра и вычисляют:
+- длину окружности по формуле D = 2 · π · R;
+- площадь круга по формуле S = π · R2;
+- объем шара. Формула V = 4/3 · π · R3.
+Методы должны быть представлены в отдельном классе как нестатические (без ключевого слова static).
+Желательно, чтобы класс был объявлен в отдельном модуле (файле).
+*/
+
+using Homework10Task2;
+
+class Program
+{
+    public delegate double Calculation(double radius);
+
+    public static void Main()
+    {
+
+        GeometricFormulas figure = new();
+        double radius = 11.1;
+
+        Calculation circleLength = figure.GetCircleLength;
+        Calculation circleSquare = figure.GetCircleSquare;
+        Calculation sphereVolume = figure.GetSphereVolume;
+
+        Console.WriteLine($"Длина окружности: {circleLength(radius):f2}");
+        Console.WriteLine($"Площадь круга: {circleSquare(radius):f2}");
+        Console.WriteLine($"Объём шара: {sphereVolume(radius):f2}");
+    }
+}
