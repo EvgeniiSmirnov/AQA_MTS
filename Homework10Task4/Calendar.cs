f579f90ec06fb489283029d49b8575356ad868c2
HappyLemon <sm.itsmyworld@gmail.com>
2024-01-22 00:18:54 +0500

    + task 4

---
+++ Homework10Task4/Calendar.cs
@@ -0,0 +1,30 @@
+﻿namespace Homework10Task4;
+
+internal class Calendar
+{
+    public delegate void EventDelegate(Event e);
+    public event EventDelegate? Notification;
+    private List<Event> EventsList { get; set; } = new();
+
+    /// <summary>
+    /// Метод добавляет событие в массив событий (календарь)
+    /// </summary>
+    /// <param name="e"></param>
+    public void AddEvent(Event e)
+    {
+        EventsList.Add(e);
+    }
+
+    /// <summary>
+    /// Метод выбирает события для текущей даты
+    /// </summary>
+    public void TodayEventNotification()
+    {
+        foreach (Event e in EventsList)
+            // в условии задачи привязка только по дате
+            if (DateOnly.FromDateTime(DateTime.Now).Equals(DateOnly.FromDateTime(e.Date)))
+            {
+                Notification?.Invoke(e);
+            }
+    }
+}
---
+++ Homework10Task4/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework10Task4;
+
+internal class Event(string title, DateTime date, string description)
+{
+    public string Title { get; set; } = title;
+    public DateTime Date { get; set; } = date;
+    public string Description { get; set; } = description;
+
+    /// <summary>
+    /// Метод выводит данные о событии в консоль
+    /// </summary>
+    /// <param name="e"></param>
+    public static void PrintEventInfo(Event e)
+    {
+        Console.WriteLine($"Дата: {DateOnly.FromDateTime(e.Date)}, событие: {e.Title}, описание: {e.Description}");
+    }
+}
