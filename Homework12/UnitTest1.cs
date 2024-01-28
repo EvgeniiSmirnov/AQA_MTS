dcfec7221169970a0b61369f727146875777ca83
HappyLemon <sm.itsmyworld@gmail.com>
2024-01-28 14:34:32 +0500

    + NUnit homework branch

---
+++ Homework12/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Homework12
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
