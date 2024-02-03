c21ddaf879728a908c916a7ec47c97f7114c020c
HappyLemon <sm.itsmyworld@gmail.com>
2024-02-04 01:08:03 +0500

    + task 3

---
+++ Homework11Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задание 3:
+Дана строковая последовательность. Строки последовательности содержат только заглавные буквы латинского алфавита.
+Отсортировать последовательность по возрастанию длин строк, а строки одинаковой длины – по убыванию.
+*/
+
+class Programm
+{
+    public static void Main()
+    {
+        List<string> stringList =
+            [
+                "ABB",
+                "AAA",
+                "BBB",
+                "CCC",
+                "ADVL",
+                "QWEQASD",
+                "ASDASDA",
+                "F"
+            ];
+
+        var sortedStringList = stringList.OrderBy(s => s.Length).ThenByDescending(s => s);
+
+        // вывод элементов в консоль
+        foreach (var num in sortedStringList) Console.Write($"{num} ");
+    }
+}
