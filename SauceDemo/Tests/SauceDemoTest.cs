c433ddc73a44c6f083cfb831cb0b4a9e64ba2fc0
HappyLemon <sm.itsmyworld@gmail.com>
2024-02-10 15:28:43 +0500

    + xpath

--- SauceDemo/Tests/SauceDemoTest.cs
+++ SauceDemo/Tests/SauceDemoTest.cs
@@ -1,7 +1,5 @@
-using AngleSharp.Dom;
 using OpenQA.Selenium;
 using SauceDemo.Helpers.Configuration;
-using System.Xml.Linq;
 
 namespace SauceDemo.Tests;
 
@@ -31,17 +29,23 @@ public class SauceDemoTest : BaseTest
     }
 
     [Test]
-    public void LocatorsCSSCheck()
+    public void LocatorsXPathCheck()
     {
         Driver.Navigate().GoToUrl(Configurator.AppSettings.URL);
 
         Assert.Multiple(() =>
         {
-
+            // ancestor, например //*[text()='Enterprise Testing']//ancestor::div
+            Assert.That(Driver.FindElement(By.XPath("//input[@id='user-name']")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[text()='Accepted usernames are:']")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//input[contains(@name,'name')]")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//h4[contains(text(),'Accepted')]")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[text()='standard_user']//ancestor::div")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='login_credentials_wrap']//descendant::h4")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='error-message-container']/following::input")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='form_group']//parent::form")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[@class='login_credentials']//preceding::form")).Displayed);
+            Assert.That(Driver.FindElement(By.XPath("//*[@type='password' and @name='password']")).Displayed);
         });
-
-        Thread.Sleep(5000);
-
-        Console.WriteLine($"{this} Finished...");
     }
 }
