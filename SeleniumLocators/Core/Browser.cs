6e1687ca5c29d4deb80fd861c19af428c0da750b
HappyLemon <sm.itsmyworld@gmail.com>
2024-02-10 12:54:06 +0500

    + selenium lacators branch

---
+++ SeleniumLocators/Core/Browser.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SeleniumLocators.Helpers.Configuration;
+
+namespace SeleniumLocators.Core;
+
+public class Browser
+{
+    public IWebDriver Driver { get; }
+
+    public Browser()
+    {
+        Driver = Configurator.BrowserType?.ToLower() switch
+        {
+            "chrome" => new DriverFactory().GetChromeDriver(),
+            "firefox" => new DriverFactory().GetFirefoxDriver(),
+            _ => Driver
+        } ?? throw new InvalidOperationException("Browser is not supported.");
+
+        Driver.Manage().Window.Maximize();
+        Driver.Manage().Cookies.DeleteAllCookies();
+        //Driver!.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+    }
+}
