2b0d348a1712077c4bfe7bdfa22fd81d034c5842
HappyLemon <sm.itsmyworld@gmail.com>
2024-02-17 02:59:20 +0500

    start draft

---
+++ PageObjectSauceDemo/Tests/BaseTest.cs
@@ -0,0 +1,27 @@
+using PageObjectSauceDemo.Core;
+using PageObjectSauceDemo.Helpers;
+using PageObjectSauceDemo.Helpers.Configuration;
+using OpenQA.Selenium;
+
+namespace PageObjectSauceDemo.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+    protected WaitsHelper WaitsHelper { get; private set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver = new Browser().Driver;
+        WaitsHelper = new WaitsHelper(Driver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
