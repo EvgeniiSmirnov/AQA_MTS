940f11c2b123d3ef7b612b8c30607b2c545c38d3
HappyLemon <sm.itsmyworld@gmail.com>
2024-02-18 02:43:19 +0500

    -

--- TestProject111/Helpers/Configuration/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace TestProject111.Helpers.Configuration;
-
-public class AppSettings
-{
-    public string? URL { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
-}
