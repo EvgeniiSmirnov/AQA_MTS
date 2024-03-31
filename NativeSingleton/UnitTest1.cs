1fd8c361e8aa59def7d9ee9410a699aa00e9c089
HappyLemon <sm.itsmyworld@gmail.com>
2024-03-31 17:19:03 +0500

    +NativeSingleton

---
+++ NativeSingleton/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NativeSingleton
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
