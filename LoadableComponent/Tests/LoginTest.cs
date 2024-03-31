f6a15f3924a8819945874d48edc41def1637da5d
HappyLemon <sm.itsmyworld@gmail.com>
2024-03-31 14:14:29 +0500

    +LoadableComponent

---
+++ LoadableComponent/Tests/LoginTest.cs
@@ -0,0 +1,28 @@
+﻿using ChainOfInvocations.Pages;
+using NUnit.Framework;
+using ChainOfInvocations.Helpers.Configuration;
+
+namespace ChainOfInvocations.Tests;
+
+public class LoginTest : BaseTest
+{
+    [Test]
+    public void SuccessfulLoginTest()
+    {
+        DashboardPage dashboardPage = _navigationSteps
+            .SuccessfulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password);
+
+        Assert.That(dashboardPage.IsPageOpened);
+    }
+
+    [Test]
+    public void InvalidUsernameLoginTest()
+    {
+        // Проверка
+        Assert.That(
+            _navigationSteps
+                .IncorrectLogin("ssdd", "123123")
+                .GetErrorLabelText(),
+            Is.EqualTo("Email/Login or Password is incorrect. Please try again."));
+    }
+}
