996a73065522e97113e1b61b1126c38191f38033
HappyLemon <sm.itsmyworld@gmail.com>
2024-03-31 13:54:05 +0500

    ChainOfInvocations+

---
+++ ChainOfInvocations/Steps/BaseSteps.cs
@@ -0,0 +1,14 @@
+﻿using ChainOfInvocations.Pages.ProjectPages;
+using ChainOfInvocations.Pages;
+using OpenQA.Selenium;
+
+namespace ChainOfInvocations.Steps;
+
+public class BaseSteps(IWebDriver driver)
+{
+    protected readonly IWebDriver Driver = driver;
+
+    protected LoginPage? LoginPage { get; set; }
+    protected DashboardPage? DashboardPage { get; set; }
+    protected AddProjectPage? AddProjectPage { get; set; }
+}
