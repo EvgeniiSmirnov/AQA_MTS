03819f4439dd99a087053139ae495f7a9bfd3f3c
HappyLemon <sm.itsmyworld@gmail.com>
2024-04-01 06:33:01 +0500

    +

--- ChainOfInvocations/Pages/ProjectPages/ProjectBasePage.cs
+++ ChainOfInvocations/Pages/ProjectPages/ProjectBasePage.cs
@@ -4,11 +4,15 @@ namespace ChainOfInvocations.Pages.ProjectPages;
 
 public class ProjectBasePage : BasePage
 {
-        private static readonly By NameInputBy = By.Id("name");
+    private static readonly By NameInputBy = By.Id("name");
     private static readonly By ProjectTypeRadioButtonBy = By.Name("suite_mode");
-    public ProjectBasePage(IWebDriver driver) : base(driver) { }
+    public ProjectBasePage(IWebDriver driver) : base(driver)
+    {
+    }
 
-    public ProjectBasePage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl) { }
+    public ProjectBasePage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
 
     protected override string GetEndpoint()
     {
