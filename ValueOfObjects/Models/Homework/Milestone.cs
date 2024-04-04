c08c4dd6de5117f0e162203a232c0239cf1e92ea
HappyLemon <sm.itsmyworld@gmail.com>
2024-04-04 14:21:53 +0500

    + homework

---
+++ ValueOfObjects/Models/Homework/Milestone.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ValueOfObjects.Models.Homework;
+
+public class Milestone
+{
+    [JsonPropertyName("id")] public int ID { get; set; }
+    [JsonPropertyName("name")] public required string MilestoneName { get; set; }
+    [JsonPropertyName("description")] public string Description { get; set; }
+    [JsonPropertyName("start_on")] public DateTime StartOn { get; set; }
+    [JsonPropertyName("started_on")] public DateTime StartedOn { get; set; }
+    [JsonPropertyName("is_started")] public bool IsStarted { get; set; }
+    [JsonPropertyName("due_on")] public DateTime DueOn { get; set; }
+    [JsonPropertyName("is_completed")] public bool IsCompleted { get; set; }
+    [JsonPropertyName("completed_on")] public DateTime CompletedOn { get; set; }
+    [JsonPropertyName("parent_id")] public int ParentId { get; set; }
+    [JsonPropertyName("project_id")] public int ProjectId { get; set; }
+    [JsonPropertyName("refs")] public string Refs { get; set; }
+    [JsonPropertyName("url")] public string URL { get; set; }
+    [JsonPropertyName("milestones")] public Milestones[] Milestones { get; set; }
+}
+public class Milestones
+{
+    [JsonPropertyName("id")] public int ID { get; set; }
+}
---
+++ ValueOfObjects/Models/Homework/ProjectModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ValueOfObjects.Models.Homework;
+
+public class ProjectModel
+{
+    [JsonPropertyName("id")] public int ID { get; set; }
+    [JsonPropertyName("name")] public required string ProjectName { get; set; }
+    [JsonPropertyName("announcement")] public string Announcement { get; set; }
+    [JsonPropertyName("show_announcement")] public bool IsShowAnnouncement { get; set; }
+    [JsonPropertyName("is_completed")] public bool IsCompleted { get; set; }
+    [JsonPropertyName("completed_on")] public int CompletedOn { get; set; }
+    [JsonPropertyName("suite_mode")] public int SuiteMode { get; set; }
+    [JsonPropertyName("default_role_id")] public int DefaultRoleId { get; set; }
+    [JsonPropertyName("case_statuses_enabled")] public bool IsCaseStatusesEnabled { get; set; }
+    [JsonPropertyName("url")] public string URL { get; set; }
+    [JsonPropertyName("users")] public ProjectUsers[] Users { get; set; }
+    [JsonPropertyName("groups")] public ProjectGroups[] Groups { get; set; }
+}
+public class ProjectUsers
+{
+    [JsonPropertyName("id")] public int ID { get; set; }
+    [JsonPropertyName("global_role_id")] public int GlobalRoleId { get; set; }
+    [JsonPropertyName("global_role")] public string GlobalRole { get; set; }
+    [JsonPropertyName("project_role_id")] public int ProjectRoleId { get; set; }
+    [JsonPropertyName("project_role")] public string ProjectRole { get; set; }
+}
+
+public class ProjectGroups
+{
+    [JsonPropertyName("id")] public int ID { get; set; }
+    [JsonPropertyName("role_id")] public int RoleId { get; set; }
+    [JsonPropertyName("role")] public string Role { get; set; }
+}
