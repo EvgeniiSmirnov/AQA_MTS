9b8b19742b69e9cfc8081671e41fa41e9f0d91a3
HappyLemon <sm.itsmyworld@gmail.com>
2024-04-16 19:50:45 +0500

    +

---
+++ DB_Npgsql/Services/CustomerService.cs
@@ -0,0 +1,42 @@
+﻿using DB_Npgsql.Models;
+using NLog;
+using Npgsql;
+
+namespace DB_Npgsql.Services;
+
+public class CustomerService
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private readonly NpgsqlConnection _connection;
+
+    public CustomerService(NpgsqlConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public List<Customer> GetAllCustomers()
+    {
+        var customers = new List<Customer>();
+
+        var cmd = new NpgsqlCommand("SELECT * FROM Customers;", _connection);
+        var reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var customer = new Customer()
+            {
+                Id = reader.GetInt32(0),
+                Firstname = reader.GetString(reader.GetOrdinal("Firstname")),
+                Lastname = reader.GetString(reader.GetOrdinal("Lastname")),
+                Email = reader.GetString(reader.GetOrdinal("Email")),
+                Age = reader.GetInt32(reader.GetOrdinal("Age")),
+            };
+
+            _logger.Info(customer);
+
+            customers.Add(customer);
+        }
+
+        return customers;
+    }
+}
---
+++ DB_Npgsql/Tests/DatabaseTest.cs
@@ -0,0 +1,35 @@
+﻿using DB_Npgsql.Connector;
+using DB_Npgsql.Services;
+using NLog;
+
+namespace DB_Npgsql.Tests;
+
+public class DatabaseTest
+{
+    private DBConnector? _dbConnector;
+    private readonly Logger _logger = LogManager.GetLogger(nameof(DatabaseTest));
+    private CustomerService? _customerService;
+
+    [OneTimeSetUp]
+    public void SetupConnection()
+    {
+        _dbConnector = new DBConnector();
+        _customerService = new CustomerService(_dbConnector.Connection);
+    }
+
+    [Test]
+    public void GetAllCustomersTest()
+    {
+        _logger.Info("Started GetAllCustomersTest");
+
+        Assert.That(_customerService!.GetAllCustomers().Count, Is.EqualTo(5));
+
+        _logger.Info("Completed GetAllCustomersTest");
+    }
+
+    [OneTimeTearDown]
+    public void TearDownConnection()
+    {
+        _dbConnector!.CloseConnection();
+    }
+}
